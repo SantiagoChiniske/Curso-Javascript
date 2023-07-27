56a17bf1d39fa25895711be218dc6cb0d494eb6d
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-07-27 20:58:35 -0300

    Aula  sobre Const

---
+++ aula07/index.js
@@ -0,0 +1,16 @@
+//Não podemos criar variáveis com palavras reservadas
+//Variáveis precisam ter nomes significativos
+//Não podemos começar o nome de uma variável com um número
+//Ultilizamos camelCase
+//Case-sensitive
+//Não pode modificar o valor de uma constante 
+//Não ultilize var , ultilize const.
+//+ - * /
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const conta = primeiroNumero * segundoNumero;
+
+console.log(conta)
+
+console.log(typeof primeiroNumero)
