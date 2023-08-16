c656a9512cf2a7cb0c845ef0b16e36ddcaa9dec4
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-08-16 20:56:07 -0300

    Operadores Aritmeticos

---
+++ aula09/index.js
@@ -0,0 +1,15 @@
+/*
+    Aritméticos
+    + Adição / concatenação
+    (- / *)
+    ** Portenciação
+    %Resto
+    ++ Encrementador
+    -- Decremento
+
+    Ordem de presendecia
+    1 - ()
+    2 - **
+    3 - * / %
+    4 - * + -
+*/
