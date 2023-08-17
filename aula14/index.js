c351996d45d6b410fc412558ef126a0dbac48712
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-08-17 11:26:04 -0300

    Sobre Numbers

---
+++ aula14/index.js
@@ -0,0 +1,8 @@
+let num1 = 14632;
+let num2 = 2.545487842;
+
+console.log(num1.toString() + num2)// transformou temporariamente o num1 em string
+console.log(num1.toString(2)) // Forma binária
+console.log(num2.toFixed(2))//Arredondado
+console.log(Number.isInteger(num1)) // Descubrir se ele é inteiro
+console.log(Number.isNaN(num1))//Descobrir se ele é um Not a number
