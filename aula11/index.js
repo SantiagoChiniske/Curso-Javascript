3518a7bdc6c6ef3ff787e235883adadf05423d2e
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-08-17 07:58:37 -0300

    Exercício de conta
    Fazer uma conta pelo window alert

---
+++ aula11/index.js
@@ -0,0 +1,13 @@
+/*
+window.alert('Seja bem vindo')
+window.confirm();
+window.prompt('Digite seu nome');
+ */
+
+window.alert(' Vamos fazer uma conta:');
+let num1 = parseInt(window.prompt('Digite um valor')) ;
+let num2 = parseInt(window.prompt('Digite segundo valor'))
+
+result = num1 + num2
+
+window.alert('O resultado foi '+ result)
