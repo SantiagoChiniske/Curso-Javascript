067b7e20341fe47ae132092b8c358e323b279984
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-08-24 12:49:24 -0300

    Objeto Math

---
+++ aula15/index.js
@@ -0,0 +1,15 @@
+let num1 = 9.54578;
+let num2  = Math.floor(num1); //arredonda para baixo
+let num3  = Math.ceil(num1); // arredondad para cima
+let num4  = Math.round(num1); // Vai arredondarpara o mais próximo
+
+
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(Math.max(1,2,3,55,44,22,39,85,-5)) //Ele verifica o valor mais alto
+console.log(Math.min(1,2,3,55,44,22,39,85,-5))//ele verifica o menor valor
+const aleatorio= Math.random()* (10-5)+5; //Numero aleatorios
+console.log(num2 **0.5) //Calculo da raiz ou pode trocar o 0.5 por 1/2
+
+console.log(aleatorio)
