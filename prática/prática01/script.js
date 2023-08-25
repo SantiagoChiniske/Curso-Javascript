5cc0863cb9df783150ecb1813817df02598733f9
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-08-25 14:52:09 -0300

    Projeto pessoal

---
+++ prática/prática01/script.js
@@ -0,0 +1,10 @@
+const valor1 = document.getElementById('valor1')
+const valor2 = document.getElementById('valor2')
+const operdaores = document.getElementById('operadores')
+const resultado = document.getElementById('resultado')
+
+function conta(valor1,operadores,valor2){
+ 
+    switch(operadores)
+
+}
