e17dbf5f60a949e6cfe34682cdfd334abf2ce801
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-08-30 15:05:50 -0300

    Operadores lógicos

---
+++ aula22/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores lógicos 
+&& -> And -> E - > Todas as expressões precisam ser verdadeiras para retonar TRUE
+||  -> Or -> Ou -> Aqui - Uma expressão precisa ser verdadeira para retornar TRUE
+! -> NOT -> Não
+*/
+
+const expressaoAnd = true && true && true
+const expressaoOr = false || true
+
+console.log(expressaoAnd)
+console.log(expressaoOr)
+console.log(!true)
