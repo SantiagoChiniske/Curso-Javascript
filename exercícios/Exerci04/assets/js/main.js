5dc8d8173db6442f5f8794d12c63465f63495e47
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-09-04 21:12:04 -0300

    Programa de IMC

--- exercícios/Exerci04/assets/js/main.js
+++ exercícios/Exerci04/assets/js/main.js
@@ -1,29 +1,71 @@
-function meuEscopo(){
-const form = document.querySelector('.container');
-const resultado = document.querySelector('.resultado');
+const form = document.querySelector('.form');
 
-function calcularImc (evento){
-    evento.preventDefault();
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    const inputPeso = event.target.querySelector('#peso')
+    const inputAltura = event.target.querySelector('#altura')
 
-    const peso = container.querySelector('#peso')
-    const altura = container.querySelector('#altura')
+    const peso = Number(inputPeso.value);
+    const altura= Number(inputAltura.value);
+   
+    if (!peso){
+        setResultado('Peso inválido',false);
+        return;
+    }
+
+    if(!altura){
+        setResultado('Altura inválido',false);
+        return;
+    }
+
+    const imc = getImc(peso,altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+    setResultado(msg,true)
+} );
+
+function getNivelImc(imc){
+
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1',
+    'Obesidade grau 2','Obesidade grau 3' ];
+
+    if(imc >= 39)return nivel[5]; 
+    if(imc >= 34.9)return nivel[4];
+    if(imc >= 29.9)return nivel[3];
+    if(imc >= 24.9)return nivel[2];
+    if(imc >= 18.5 )return nivel[1];
+    if(imc <18.5)return nivel[0];
+}
+
+function getImc(peso,altura){
+    const imc = peso/altura **2;
+    return imc.toFixed(2);
+}
+
+function criaP(){
+    const p = document.createElement('p'); // Cria um elemento HTML
+    //p.classList.add('paragrafo-resultado') Colocar uma classe nele
+    return p;
+}
 
 
-    resultado = (altura*altura)/peso;
+function setResultado(msg,isValid){
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = '';
+    
+    
 
-    console.log(resultado)
-    resultado.innerHTML = `<p>Nome:${imc.value} </p>`
+    const p = criaP();
 
+    if(isValid) {
+        p.classList.add('paragrafo-resultado');
+    }else{
+        p.classList.add('bad');
+    }
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 
+}
 
-};
-
-
-form.addEventListener('submit', calcularImc )
-
-};
-
-
-
-
-meuEscopo();
