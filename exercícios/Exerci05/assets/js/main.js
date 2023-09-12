4fdd27a25b24a983dffd3b96f83b3c937e71b17b
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-09-11 22:01:30 -0300

    Projeto relógio e dia

--- exercícios/Exerci05/assets/js/main.js
+++ exercícios/Exerci05/assets/js/main.js
@@ -1,9 +1,6 @@
 const h1 = document.querySelector('.container h1');
 const data = new Date();
 
-
-
-
 function getDiaSemanaTexto(diaSemana){
     let diaSemanaTexto;
 
@@ -112,3 +109,7 @@ function criaData(data){
 }
 
 h1.innerHTML = criaData(data);
+
+// const h1 = document.querySelector('.container h1');
+// const data = new Date();
+// h1.innerHTML = data.toLocaleDateString('pt-BR', {dataStyle: 'full', timeStyle:'Short');
