f00ed577995f300a79b5bac997d4f72b9ffc7151
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-09-11 22:01:20 -0300

    update index.js

--- aula29/index.js
+++ aula29/index.js
@@ -1,27 +1,7 @@
 const data = new Date();
 let diaSemana = data.getDay();
-//diaSemana = 8;
 let diaSemanaTexto;
-/*
-if (diaSemana === 0){
-    diaSemanaTexto = 'Domingo'
-}else if(diaSemana ===1){
-    diaSemanaTexto ='Segunda'
-}else if(diaSemana ===2){
-    diaSemanaTexto ='Terça'
-}else if(diaSemana ===3){
-    diaSemanaTexto ='Quarta'
-}else if(diaSemana ===4){
-    diaSemanaTexto ='Quinta'
-}else if(diaSemana ===5){
-    diaSemanaTexto ='Sexta'
-}else if(diaSemana ===6){
-    diaSemanaTexto ='Sábado'
-}else{
-    diaSemanaTexto = 'Valor incorreto'
-}
-console.log(diaSemana,diaSemanaTexto);
-*/
+
 switch(diaSemana){
     case 0:
         diaSemanaTexto = 'Domingo'
@@ -58,5 +38,25 @@ switch(diaSemana){
 }
 
 console.log(diaSemana, diaSemanaTexto);
-    
-    
+
+//diaSemana = 8;
+/*
+if (diaSemana === 0){
+    diaSemanaTexto = 'Domingo'
+}else if(diaSemana ===1){
+    diaSemanaTexto ='Segunda'
+}else if(diaSemana ===2){
+    diaSemanaTexto ='Terça'
+}else if(diaSemana ===3){
+    diaSemanaTexto ='Quarta'
+}else if(diaSemana ===4){
+    diaSemanaTexto ='Quinta'
+}else if(diaSemana ===5){
+    diaSemanaTexto ='Sexta'
+}else if(diaSemana ===6){
+    diaSemanaTexto ='Sábado'
+}else{
+    diaSemanaTexto = 'Valor incorreto'
+}
+console.log(diaSemana,diaSemanaTexto);
+*/
