fd718f994e27c884332b23452b7db4a5eded7b53
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-09-18 20:01:06 -0300

    For clásico

---
+++ aula34/index.js
@@ -0,0 +1,17 @@
+//for clásico
+// i = index
+for(let i = 0 ; i <= 10 ; i++){
+    console.log(`Linha ${i}`)
+
+}
+
+for (let i = 0; i<= 10; i++){
+    const par = i % 2 === 0 ? 'par' : 'Ímpar'
+    console.log(i, par) 
+}
+
+const fruta = ['Uva', 'Maça', 'Pera', 'Banana']
+
+for (i = 0 ; i < fruta.length; i++){
+    console.log(fruta[i])
+}
