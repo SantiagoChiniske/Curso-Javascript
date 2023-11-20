f36874cd0df24f680ba9cbd56e7c2bd3af1c225e
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-11-20 12:46:30 -0300

    Aula sobre for

---
+++ aula37/index.js
@@ -0,0 +1,23 @@
+const nome = ['Santiago Chiniske', 'Marcos José','Pedro Augusto'];
+
+for (let i = 0; i< nome.length; i++){
+    console.log(nome[i]);
+}
+
+console.log('##########')
+
+for (let i in nome){
+    console.log(nome[i]);
+}
+
+console.log('##########')
+
+for ( let valor of nome){
+    console.log(valor)
+}
+
+console.log('##########')
+
+nome.forEach(function(valor,indice){
+    console.log(valor, indice)
+})
