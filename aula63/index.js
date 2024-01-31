e35afd04a2fce6fe1d83cdeb6b97329e85afc9da
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2024-01-31 20:31:15 -0300

    Aula sobre split

---
+++ aula63/index.js
@@ -0,0 +1,23 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia']
+
+//nomes.splice (índice,delete, elem1,elem2)
+//const removidos = nomes.splice(4,1)
+//const removidos = nomes.splice(-2,1)//Pode colocar no 1 Number.MAX_VALUE que ele vai até o máximo do elemento
+//const removidos = nomes.splice(4,0,'Santiago') // Adiciona um elemento
+
+//pop
+//const removidos = nomes.splice(-1,1);
+
+//Shift
+//const removidos = nomes.splice(0,1);
+
+//push
+//const removidos1 = nomes.splice(nomes.length,0,'Santiago');
+
+//unshift
+//const removidos1 = nomes.splice(0,0,'Santiago');
+
+
+console.log(nomes)
+//console.log(removidos)
+
